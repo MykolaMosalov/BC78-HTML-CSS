15823be3a832868ba25b36fb4d4f8bbd5af9900e
MykolaMosalov <137917653+MykolaMosalov@users.noreply.github.com>
2025-10-03 11:55:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-const modalBtn = document.querySelector(".hero-btm")
-const modal = document.querySelector(".modal")
-modalBtn.addEventListener("click", () => {
-    modal.classList.toggle("is-active")
-})
+const modalBtnEl = document.querySelector('.hero-btm');
+const backdropEl = document.querySelector('.backdrop');
+const closeBtnEl = document.querySelector('.modal-close-btn');
+const bodyEl = document.body;
+
+modalBtnEl.addEventListener('click', toggleModal);
+closeBtnEl.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  backdropEl.classList.toggle('is-open');
+
+  if (backdropEl.classList.contains('is-open')) {
+    bodyEl.style.overflow = 'hidden';
+  } else {
+    bodyEl.style.overflow = 'visible';
+  }
+}
